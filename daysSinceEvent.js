2a3ffce8abd5059de8cd9389511a910090fc371a
Barcelona Scrum <barcelona.scrum@gmail.com>
2016-11-28 20:04:23 +0100

    JavaScript refactor

--- daysSinceEvent.js
+++ daysSinceEvent.js
@@ -1,42 +1,107 @@
-(function daysSinceEvent() {
-    var numberOfDaysElement = document.getElementById("numberOfDays");
-    var descriptionElement = document.getElementById("eventDescription");
-    var eventDateString = getQueryVariable("eventDate");
-    var eventDescriptionString = getQueryVariable("eventDescription");
-    var daysSinceEvent = getDaysSinceDate(eventDateString);
+let state = {
+    limits: [ {
+        daysOver: 0,
+        textColor: "#000",
+        bgColor: "#66cc66"
+    },
+    {
+        name: "daysBeforeWarning",
+        daysOver: 10,
+        textColor: "#000",
+        bgColor: "#3399ff"
+    },
+    {
+        name: "daysBeforeAlerting",
+        daysOver: 20,
+        textColor: "#000",
+        bgColor: "#ff0033"
+    } ],
+    startDate: Date.now(),
+    description: "something"
+};
 
-    if (typeof eventDateString !== "undefined") {
-        numberOfDaysElement.innerHTML = daysSinceEvent;
+const modifyLimit = (name, limits, newValue) => {
+    let limit = limits.find((limit) => {
+        return limit.name !== undefined && limit.name === name
+    });
+    limit.daysOver = newValue;
+    return [
+       ...limits.slice(undefined, limits.indexOf(limit)),
+       limit,
+       ...limits.slice(limits.indexOf(limit)+1, undefined)
+    ];
+};
+
+const getCurrentLimit = (state) => {
+    return state.limits.reduce((result, limit) => {
+        if (getNumberOfDaysElapsed(state.startDate) >= limit.daysOver) {
+            result = limit;
+        }
+        return result;
+    })
+};
+
+const getNumberOfDaysElapsed = (startDate) => {
+    return Math.floor((new Date() - startDate)/(1000*60*60*24));
+}
+
+const queryKeys = {
+    "eventDate": (state, value) => {
+        return Object.assign({}, state, { startDate: new Date(value) });
+    },
+    "eventDescription": (state, value) => {
+        return Object.assign({}, state, { description: value });
+    },
+    "daysBeforeWarning": (state, value) => {
+        return Object.assign({}, state, {
+            limits: modifyLimit("daysBeforeWarning", state.limits, value)
+        });
+    },
+    "daysBeforeAlerting": (state, value) => {
+        return Object.assign({}, state, {
+            limits: modifyLimit("daysBeforeAlerting", state.limits, value)
+        });
     }
-    if (typeof eventDescriptionString !== "undefined") {
-        descriptionElement.innerHTML = eventDescriptionString;
-    }
+};
 
-    var daysBeforeAlertingInt = parseInt(getQueryVariable("daysBeforeAlerting"));
-    var daysBeforeWarningInt = parseInt(getQueryVariable("daysBeforeWarning"));
-    if (daysSinceEvent > daysBeforeAlertingInt) {
-    	numberOfDaysElement.parentElement.classList.add("alert");
-    } else if (daysSinceEvent > daysBeforeWarningInt) {
-    	numberOfDaysElement.parentElement.classList.add("warning");
-    }
-
-    function getDaysSinceDate(eventDateString) {
-
-        var eventDate = new Date(eventDateString);
-        var currentDate = new Date();
-        var daysSinceEvent = Math.floor((currentDate - eventDate)/(1000*60*60*24));
-        return daysSinceEvent;
-    }
-
-    function getQueryVariable(variable) {
-
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-        for (var i=0;i<vars.length;i++) {
-            var pair = vars[i].split("=");
-            if (pair[0] == variable) {
-                return unescape(pair[1]);
-            }
-        } 
-    }
+const queryParameters = (() => {
+    const query = window.location.search.split("#");
+    const queryParams = query[0].substring(1);
+    const queryHash = query[1];
+    return queryParams.split("&").reduce((result, str) => {
+        const keyValue = str.split("=");
+        if (keyValue[1] !== undefined) {
+            result[keyValue[0]] = unescape(keyValue[1]);
+        }
+        return result;
+    }, {});
 })();
+
+const elements = {
+    "counter": () => {
+        const daysElapsed = getNumberOfDaysElapsed(state.startDate);
+        document.getElementById("numberOfDays").textContent = daysElapsed;
+    },
+    "description": () => {
+        document.getElementById("eventDescription").textContent = state.description;
+    },
+    "numberColor": () => {
+        const numberOfDays = getNumberOfDaysElapsed(state.startDate);
+        const limit = getCurrentLimit(state);
+        let numberElement = document.getElementById("numberOfDays");
+
+        numberElement.parentElement.style.backgroundColor = limit.bgColor;
+        numberElement.parentElement.style.color = limit.textColor;
+    }
+};
+
+(() => {
+    Object.keys(queryParameters).filter((key) => {
+        return key in queryKeys;
+    }).forEach((key) => {
+        state = queryKeys[key](state, queryParameters[key]);
+    });
+
+    Object.keys(elements).forEach((key) => elements[key]());
+})();
+
